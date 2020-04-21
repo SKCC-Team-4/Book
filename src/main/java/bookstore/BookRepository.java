04aaab7015c7cd82a29ec1a66ffcb552a8cda242
balmung1 <balmung1@naver.com>
2020-04-21 16:44:49 +0900

    .

--- src/main/java/bookstore/BookRepository.java
+++ src/main/java/bookstore/BookRepository.java
@@ -1,8 +1,10 @@
 package bookstore;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends PagingAndSortingRepository<Book, Long>{
-
 
 }
